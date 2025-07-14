a37571bf8810360128f7d39b647d8fd4a7b92c4b
abhinav kumar <abhinav.mg.k@gmail.com>
2025-07-14 21:02:07 +0530

    updates done

--- todos/model.py
+++ todos/model.py
@@ -3,4 +3,32 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
-    
+
+    class Config:
+        schema_extra = {          # ← all-lowercase
+            "example": {          # ← all-lowercase
+                "id": 1,
+                "item": "Example Schema Item"
+            }
+        }
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Example Todo Item"
+            }
+        }
+class TodoItems(BaseModel):
+    items: list[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "items": [
+                    {"item": "Example Todo Item 1"},
+                    {"item": "Example Todo Item 2"}
+                ]
+            }
+        }
--- todos/todo.py
+++ todos/todo.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter
-from model import Todo
+from fastapi import APIRouter, Path
+from model import Todo, TodoItem, TodoItems
 todo_router = APIRouter()
 
 todo_list = []
 
-@todo_router.get("/todos")
+@todo_router.get("/todos", response_model=TodoItems)
 async def retrieve_todos() -> dict:
-    return {"todos": todo_list}
+    return {"items": todo_list}
 
 @todo_router.post("/todos")
 async def add_todo(todo: Todo) -> dict:
@@ -14,8 +14,28 @@ async def add_todo(todo: Todo) -> dict:
     return {"message": "Todo added successfully"}
 
 @todo_router.get("/todos/{todo_id}")
-async def get_todo(todo_id: int) -> dict:
+async def get_todo(todo_id: int = Path(...,title="The Id of the Todo to retrieve")) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
             return {"todo": todo}
     return {"message": "Todo not found"}
+
+@todo_router.put("/todos/{todo_id}")
+async def update_todo(todo_item: TodoItem, todo_id: int = Path(... , title="The Id of the Todo to be updated")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_item.item
+            return {"message": "Todo updated successfully"}
+    return {"message": "Todo not found"}
+@todo_router.delete("/todos/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+  for index in range(len(todo_list)):
+      if todo_list[index].id == todo_id:
+          del todo_list[index]
+          return {"message": "Todo deleted successfully"}
+  return {"message": "Todo not found"}
+
+@todo_router.delete("/todos")
+async def delete_all_todos() -> dict:
+    todo_list.clear()
+    return {"message": "All todos deleted successfully"}
